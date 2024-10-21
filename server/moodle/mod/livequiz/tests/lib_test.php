aeaea1270ac3463e90db992c40b3a129b88909cc
MShadiF <113097694+MShadiF@users.noreply.github.com>
2024-10-21 16:01:51 +0200

    Update lib_test.php

--- server/moodle/mod/livequiz/tests/lib_test.php
+++ server/moodle/mod/livequiz/tests/lib_test.php
@@ -21,6 +21,7 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 namespace mod_livequiz;
+require_once($CFG->dirroot . '/mod/livequiz/lib.php');
 /**
  * Testing examples!
  */
